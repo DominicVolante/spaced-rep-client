7c407efcf6a06221be7d3b685953ff5eab4e772c
Dominic Volante <dominicvolante@gmail.com>
2021-01-08 12:20:42 -0500

    working on deployment

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export default {
-  //API_ENDPOINT: '',
-  API_ENDPOINT: "http://localhost:8000/api",
+  API_ENDPOINT: "https://glacial-eyrie-73604.herokuapp.com/api",
+  //API_ENDPOINT: "http://localhost:8000/api",
   TOKEN_KEY: "blogful-client-auth-token",
 };
